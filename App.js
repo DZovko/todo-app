6a87bd2d2480e111d2275cb8fe6ac4d6e5fc4e29
DZovko <domagoj.zovko0004@gmail.com>
2024-04-02 04:48:51 +0200

    First commit

---
+++ App.js
@@ -0,0 +1,59 @@
+
+import { StyleSheet, Text, View, ScrollView, FlatList, TouchableOpacity } from 'react-native';
+import React,{useState} from 'react';
+import  Header  from './components/header';
+
+
+
+export default function App() {
+ const [todos,setTodos]= useState([
+  {text:'buy coffe', key:'1'},
+  {text:'create an app', key:'2'},
+  {text:'play games', key:'3'},
+
+
+ ])
+  
+  
+
+
+  return (
+  
+    <View style={styles.container}> 
+     {/*heather*/}
+     {/* komentar */}
+     <Header />
+      <View style={styles.content}>
+        {/*to do form */}
+
+        <View style={styles.list}>
+          <FlatList
+          data={todos}
+          renderItem={({item})=>(
+            <Text>{item.text}</Text>
+          )}
+          />
+        </View>
+      </View>
+    
+    </View>
+   
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+
+  },
+  content:{
+    padding: 40,
+  },
+  list:{
+    margintop: 20,
+  },
+  
+
+ 
+});
